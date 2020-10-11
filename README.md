03f93a4c43af5b50256d17f235012d885204a013
Sampsa Laapotti <sampsa@laapotti.net>
2020-10-11 13:27:26 +0300

    Add rudimentary parameter handling and instructions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # COVID-19-utils
 Utils for handling some COVID-19 related data
+
+## How to run this?
+```
+# In shell
+python3 convert.py all-exposure-checks.json
+
+# This creates output.csv, which one can open with Libreoffice:
+libreoffice output.csv
+```
--- convert.py
+++ convert.py
@@ -1,12 +1,20 @@
 """ A small script to convert covid notifications from Android phone to csv """
-import json
+import json, sys
 
-print("test")
-OUTPUT = ""
-with open('all-exposure-checks.json', 'r') as f:
-    EXPOSURE_CHECKS = json.loads(f.read())
-    HEADER = []
-    OUTPUTS = []
+# Read the input file
+try:
+    INPUT_FILE=(sys.argv[1])
+except:
+    print("No file supplied, please give filename as first argument")
+    exit(1)
+
+
+if INPUT_FILE:
+    OUTPUT = ""
+    with open(INPUT_FILE, 'r') as f:
+        EXPOSURE_CHECKS = json.loads(f.read())
+        HEADER = []
+        OUTPUTS = []
     for c in EXPOSURE_CHECKS:
         # TODO potential bug if header changes
         HEADER = c.keys()
@@ -16,8 +24,8 @@ with open('all-exposure-checks.json', 'r') as f:
     for o in OUTPUTS:
         line = ', '.join(['"{}"'.format(x) for x in o])
         LINES.append(line)
-    print("\n".join(LINES))
-    OUT = '"' + '","'.join(HEADER) + '"\n' + "\n".join(LINES)
-print(f'{OUT}')
-with open('output.csv', 'w') as f:
-    f.write(OUT)
+        print("\n".join(LINES))
+        OUT = '"' + '","'.join(HEADER) + '"\n' + "\n".join(LINES)
+        print(f'{OUT}')
+    with open('output.csv', 'w') as f:
+        f.write(OUT)
